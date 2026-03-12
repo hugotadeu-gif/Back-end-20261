10cea494529427a7880633dba39b44bccb53b93d
hugotadeu-gif <hugo.tadeu@uni9.edu.br>
2026-03-12 14:34:06 +0000

    aula 12032026

---
+++ src/pg01_1.php
@@ -0,0 +1,29 @@
+<?php
+echo "<h1>Tópico 02 </h1>";//escrever anotações
+echo "Outra Linha<br>";#anotações
+print "funciona com print<br>";
+/*
+anotar
+varias
+linhas
+*/
+//versão do PHP
+echo phpversion();
+echo"<hr>";
+$idade=25;// int
+$nota=8.5;// float
+$nome="Bete";// string
+
+echo $nome; 
+echo", você tem ";
+echo $idade; 
+echo" anos e sua nota foi "; 
+echo$nota;
+
+echo"<hr>";
+echo "$nome, voçê tem <b> $idade </b> anos e sua nota foi $nota !";
+echo "<hr>";
+var_dump ($nome)
+
+?>
+<p> Alguma coisa </p>
